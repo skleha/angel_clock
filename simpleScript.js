60ebec2d43a2f77bf719b7b7f84df73083f26ae6
thejenkdog <thejenkdog@gmail.com>
2019-12-17 14:59:41 -0800

    Functional angle calculator, but with separate input boxes

--- simpleScript.js
+++ simpleScript.js
@@ -1,18 +1,20 @@
 
 
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  
-
-
-})
-
 function angleCalc() {
   const hour = document.getElementById("hours").value;
   const minutes = document.getElementById("minutes").value;
+  const answer = document.getElementById("answer");
+
+  const hourPercent = (hour % 12) / 12;
+  const minutePercent = minutes / 60;  
   const oneHourDegrees = 360 / 24;
   
-  console.log((hour % 12) / 12 * 360);
-  console.log(minutes / 60 * 360);
+  const hourAngle = hourPercent * 360 + minutePercent * oneHourDegrees;
+  const minuteAngle = minutePercent * 360;
+
+  answer.innerHTML = Math.abs(minuteAngle - hourAngle);
+  
+  console.log(hourAngle);
+  console.log(minuteAngle);
+  console.log(Math.abs(minuteAngle - hourAngle));
 }
