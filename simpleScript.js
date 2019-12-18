152ac615dd33eb497f4de1069296f2c3e8a9423f
thejenkdog <thejenkdog@gmail.com>
2019-12-17 17:49:58 -0800

    Refactor angleCalc and create helper functions for hourAngle and minute Angle

--- simpleScript.js
+++ simpleScript.js
@@ -1,16 +1,8 @@
 
 function angleCalc() {
   const time = document.getElementById("hours").value;
-  const timeArray = time.split(":");
-  const hour = timeArray[0];
-  const minutes = timeArray[1];
-
-  const hourPercent = (hour % 12) / 12;
-  const minutePercent = minutes / 60;  
-  const oneHourDegrees = 360 / 24;
-  
-  const hourAngle = hourPercent * 360 + minutePercent * oneHourDegrees;
-  const minuteAngle = minutePercent * 360;
+  const hourAngle = hourAngle(time);
+  const minuteAngle = minuteAngle(time);
 
   const answer = Math.abs(hourAngle - minuteAngle).toFixed(1);
   const answerOneDiv = document.getElementById("answerOne");
@@ -20,3 +12,23 @@ function angleCalc() {
   answerTwoDiv.innerHTML = (360 - answer).toFixed(1);
 
 }
+
+
+function hourAngle(time) {
+  const timeArray = time.split(":");
+  const hour = timeArray[0];
+  const minutes = timeArray[1];
+  const hourPercent = (hour % 12) / 12;
+  const minutePercent = minutes / 60;
+  const hourAngle = hourPercent * 360 + minutePercent * oneHourDegrees;
+  return hourAngle;
+}
+
+function minuteAngle(time) {
+  const timeArray = time.split(":");
+  const minutes = timeArray[1];
+  const minutePercent = minutes / 60;
+  const minuteAngle = minutePercent * 360;
+  return minuteAngle;
+}
+
