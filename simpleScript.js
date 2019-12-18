8fd61b92e4e73b162940d257fad7288644f15323
thejenkdog <thejenkdog@gmail.com>
2019-12-17 18:56:01 -0800

    Add handPoint function which says where the second point on the hour, minute hand should be

--- simpleScript.js
+++ simpleScript.js
@@ -1,8 +1,8 @@
 
 function angleCalc() {
   const time = document.getElementById("hours").value;
-  const hourAngle = hourAngle(time);
-  const minuteAngle = minuteAngle(time);
+  const hourAngle = calcHourAngle(time);
+  const minuteAngle = calcMinuteAngle(time);
 
   const answer = Math.abs(hourAngle - minuteAngle).toFixed(1);
   const answerOneDiv = document.getElementById("answerOne");
@@ -13,22 +13,30 @@ function angleCalc() {
 
 }
 
-
-function hourAngle(time) {
+function calcHourAngle(time) {
   const timeArray = time.split(":");
   const hour = timeArray[0];
   const minutes = timeArray[1];
   const hourPercent = (hour % 12) / 12;
   const minutePercent = minutes / 60;
-  const hourAngle = hourPercent * 360 + minutePercent * oneHourDegrees;
-  return hourAngle;
+  const oneHourDegrees = 360 / 12;
+  const angle = hourPercent * 360 + minutePercent * oneHourDegrees;
+  return angle;
 }
 
-function minuteAngle(time) {
+function calcMinuteAngle(time) {
   const timeArray = time.split(":");
   const minutes = timeArray[1];
   const minutePercent = minutes / 60;
-  const minuteAngle = minutePercent * 360;
-  return minuteAngle;
+  const angle = minutePercent * 360;
+  return angle;
+}
+
+function handPoint(angle, length) {
+  const hourHandLength = length;
+  const radians = angle * (Math.PI / 180)
+  const x = parseFloat((hourHandLength * Math.sin(radians)).toFixed(1));
+  const y = parseFloat((hourHandLength * Math.cos(radians)).toFixed(1));
+  return {xPoint: x, yPoint: y};
 }
 
